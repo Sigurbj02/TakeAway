b1d36df84283094230e1c8523551f8a395f55ea1
Sigurbjörg Erla <sepbiering@gmail.com>
2023-04-15 18:39:59 +0000

    removed mistakes from test class

--- src/test/java/hi/vinnsla/MenuTest.java
+++ src/test/java/hi/vinnsla/MenuTest.java
@@ -16,16 +16,7 @@ public class MenuTest {
 
     @Test
     public void setMenuData() {
-        String menuString = "Pestópasta, 10900, Spagettí, 290, íslensk kjötsúpa, 1500, Pepperóní pizza, 2319, " +
-                "Margaríta pizza, 2187, Hundamatur, 200, Einn banani, 3000, mjólk, 1090, " +
-                "súrsaðir hrútspungar, 11, páfagaukavængir, 2500, DIY tómatsúpa, 2390, brjóstamjólkurís , 9999, " +
-                "pulsa í brauði, 600, pylsa í brauði, 3400";
-        String[] menuArray = menuString.split(", ");
-        for (int i = 0; i < menuArray.length - 1; i = i + 2) {
-            if (menuArray[i].length() <= 30) {
-                products.add(new Product(menuArray[i], Integer.parseInt(menuArray[i + 1])));
-            }
-        }
+       
     }
 
     @Test
