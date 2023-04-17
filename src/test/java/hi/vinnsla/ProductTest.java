442919d1c86c082ccaf9aa8365eb53a4560706e7
Sigurbjörg Erla <sepbiering@gmail.com>
2023-04-17 16:55:15 +0000

    added to ProductTest

--- src/test/java/hi/vinnsla/ProductTest.java
+++ src/test/java/hi/vinnsla/ProductTest.java
@@ -3,33 +3,37 @@ package hi.vinnsla;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ProductTest {
-    private String productName;
-    private int productPrice;
+import static org.junit.Assert.*;
 
+
+public class ProductTest {
     private Product product;
 
     @Before
     public void setUp() throws Exception {
-        productName = "Pepperoni pizza";
-        productPrice = 450;
-        product = new Product(productName, productPrice);
-        assert (product.getPrice() == productPrice);
-        assert (product.getProduct().equals(productName));
+        product = new Product("Pepperoni pizza", 1290);
+        assertEquals(product.getProduct(), "Pepperoni pizza");
+        assertEquals(product.getPrice(), 1290);
     }
 
     @Test
-    public void getPrice() {
-
+    public void testGetProduct() {
+        assertNotNull(product.getProduct());
+        assertEquals("Pepperoni pizza", product.getProduct());
+        //if there was a setter setProduct in Product, we would set it to something different and make sure it's correct
+        //what if product is null? should we check that?
     }
 
     @Test
-    public void testListViewFormat() {
-
+    public void testGetPrice() {
+        assertTrue(product.getPrice() > 0);
+        assertEquals(product.getPrice(), 1290);
     }
 
     @Test
     public void testToString() {
-        assert (product.toString().equals(productName + ", " + productPrice));
+        assertNotNull(product.toString());
+        assertEquals(product.toString(), product.getProduct() + ", " + product.getPrice());
+        //or assertEquals(product.toString(), "Pepperoni pizza, 1290");
     }
 }
