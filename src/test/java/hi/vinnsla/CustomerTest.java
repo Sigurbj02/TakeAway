4ec4960e2e02b637d36b9f6791ee060717b73ce6
Sigurbjörg Erla <sepbiering@gmail.com>
2023-04-17 16:40:32 +0000

    added to CustomerTest

--- src/test/java/hi/vinnsla/CustomerTest.java
+++ src/test/java/hi/vinnsla/CustomerTest.java
@@ -1,26 +1,35 @@
 package hi.vinnsla;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 
 public class CustomerTest {
     private Customer customer;
 
     @Before
     public void createCustomer() throws Exception {
-        customer = new Customer("nameExample", "addressExample");
-    }
-
-    @After
-    public void tearDown() throws Exception {
+        customer = new Customer("James", "12 Oak Lane");
+        assertEquals("James", customer.nameProperty().get());
+        assertEquals("12 Oak Lane", customer.addressProperty().get());
     }
 
     @Test
     public void nameProperty() {
+        assertNotNull(customer.nameProperty());
+        assertEquals("James", customer.nameProperty().get());
+        customer.nameProperty().set("Anna");
+        assertEquals("Anna", customer.nameProperty().get());
     }
 
     @Test
     public void addressProperty() {
+        assertNotNull(customer.addressProperty());
+        assertEquals("12 Oak Lane", customer.addressProperty().get());
+        customer.addressProperty().set("123 Sesame Street");
+        assertEquals("123 Sesame Street", customer.addressProperty().get());
     }
 }
